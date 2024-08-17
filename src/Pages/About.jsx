165c63019d7442414a7f1bd8ddb6c84816f8312f
askat <abdurasulov@gmail.com>
2024-08-17 15:45:03 +0600

    asdaa

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -8,9 +8,14 @@ function About() {
       <img src="https://cms.imgworlds.com/assets/9558de9d-1e49-437e-aa7b-b8bd4d999b00.jpg?key=home-gallery" alt="" />
       jsdykjlz;dvlxv.,xvn
       <button></button>
-      About askat
+      About askatA
+      yudkfhlkagjbzdzfdfdg,dfgndfg,ndbg
+     badfl/ds.fk,bndsmddhdghdhdfdfgfert'ielt;<object data="" type=""></object> 
+     a/lkm;d,h;.dsdgth;/ad,. geg
       adasfsekoovjcos
       <button></button>
+      fckSUYO';AS"lA
+      FGDGRG
       <h1></h1>
       <data value=""></data>
     </div>
